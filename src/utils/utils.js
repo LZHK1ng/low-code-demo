bb5e8c5b3f39dcd8d00ca2a2ee33f4199d7eda66
LZHK1ng <1160313432@qq.com>
2023-10-25 10:26:50 +0800

    fix: 修复了单个组件无法复制多个的问题

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,13 +1,14 @@
 export function deepClone(target) {
   if (typeof target == 'object') {
     const result = Array.isArray(target) ? [] : {}
-    for (const key in result) {
+    for (const key in target) {
       if (typeof target[key] == 'object') {
         result[key] = deepClone(target[key])
       } else {
         result[key] = target[key]
       }
     }
+    return result
   }
   return target
 }
