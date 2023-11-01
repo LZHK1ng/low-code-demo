12657f10be4ed3f05f78adde563d0653f28f6c44
LZHK1ng <1160313432@qq.com>
2023-11-01 21:35:02 +0800

    fix(utils/style.js): 部分样式不生效

--- src/utils/style.js
+++ src/utils/style.js
@@ -5,6 +5,9 @@ export default function getStyle(style, filter=[]) {
     'height',
     'top',
     'left',
+    'borderWidth',
+    'letterSpacing',
+    'borderRadius',
   ]
   const result = {}
   Object.keys(style).forEach(key => {
